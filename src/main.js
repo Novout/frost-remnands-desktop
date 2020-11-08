1d575dd8c775fddc0f5ba1c26537d75ed00e1d81
Novout <novout@hotmail.com>
2020-11-08 14:04:49 -0300

    chore(awesome): implement vue icons

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import { createPinia } from "pinia";
 import { router } from "./router";
 import App from "./App.jsx";
 import "./css/_preset.css";
+import { FontAwesomeIcon } from "@/plugins/fontawesome";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.component("fa", FontAwesomeIcon)
 app.mount("#app");
---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import FontAwesomeIcon from "@/lib/FontAwesomeIcon.vue";
+
+library.add(faUserSecret);
+
+export { FontAwesomeIcon };
