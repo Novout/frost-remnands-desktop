7615db2cbebf2e5ccec57ffec2c0d40c484f7902
Novout <novout@hotmail.com>
2020-11-12 00:50:52 -0300

    chore(env): added frost env

---
+++ frostremnands.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  /*
+   *  offline = Offline First
+   *  online = Hability general requests
+   */
+  target: "offline",
+  /*
+   *  false = disabled window chromium reload
+   *  true = window chromium reload with Ctrc+R
+   */
+  reload: false,
+  /*
+   *  false = disabled dev console
+   *  true = console in dev mode
+   */
+  console: true,
+  /*
+   *  actually website redirect
+   */
+  website: "https://novout.github.io/frost-remnands/"
+}
--- src/background.js
+++ src/background.js
@@ -4,6 +4,7 @@ import { app, protocol, BrowserWindow, globalShortcut } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 import path from 'path'
+import fr from '../frostremnands.config'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -24,19 +25,16 @@ const createWindow = () => {
     minHeight: 550,
     frame: false,
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      enableRemoteModule: true
     },
     icon: path.join(__static, 'icon.png')
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
   } else {
     createProtocol('app')
-    // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
 
@@ -46,38 +44,29 @@ const createWindow = () => {
 }
 
 app.on("ready", () => {
-  globalShortcut.register("CmdOrCtrl+R", () => {});
+  if(!fr.reload) globalShortcut.register("CmdOrCtrl+R", () => {});
   
-  if(isDevelopment) {
+  if(isDevelopment && fr.console) {
     globalShortcut.register("CmdOrCtrl+D", () => {
-      // @ts-ignore
       win.webContents.openDevTools();
     });
   }
 });
-// Quit when all windows are closed.
+
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
     try {
       await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
@@ -87,7 +76,6 @@ app.on('ready', async () => {
   createWindow()
 })
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
