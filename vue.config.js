091ee2d722893d641db4098fbfb7fbab28694cb8
Novout <novout@hotmail.com>
2020-11-11 23:26:41 -0300

    fix: build dist

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,7 @@ module.exports = {
       nodeIntegration: true,
       builderOptions: {
         productName: "Restos da Geada - RPG"
-      },
-      mainProcessFile: "src/electron/background.js"
+      }
     }
   }
 };
