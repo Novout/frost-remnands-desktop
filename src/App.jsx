7d4d6456fd229bf107346632423ec21a1b9e192a
Novout <novout@hotmail.com>
2020-11-28 02:07:26 -0300

    feat(theme): bar color

--- src/App.jsx
+++ src/App.jsx
@@ -32,14 +32,14 @@ export default defineComponent({
   render() {
     return (
       <>
-        <nav id="bar" class="flex justify-end items-center fixed bg-default-blueDark text-default-white w-full h-bar drag z-max">
+        <nav id="bar" class="flex justify-end items-center fixed bg-default-blueLight text-default-black dark:bg-default-blueDark dark:text-default-white w-full h-bar drag z-max">
           <router-link to="/" class="font-ralewayMedium text-sm mr-auto ml-10 no-underline appearance-none cursor-pointer no-drag hover:text-white-oneHover">Menu Inicial</router-link>
           <router-link to="/profile" class="font-ralewayMedium text-sm mr-auto ml-10 no-underline appearance-none cursor-pointer no-drag hover:text-white-oneHover">Perfil</router-link>
-          <div id="minimize" onClick={this.windowMinimize} class="h-full px-px10 no-drag cursor-pointer hover:bg-default-blueLight">
-            <span class="no-drag text-xl font-ralewayTiny">-</span>
+          <div id="minimize" onClick={this.windowMinimize} class="h-full px-px10 no-drag cursor-pointer hover:bg-default-blueDark dark:hover:bg-default-blueLight">
+            <span class="no-drag text-xl font-ralewayTiny text-default-black dark:text-default-white">-</span>
           </div>
-          <div id="close" onClick={this.windowClose} class="h-full px-px10 no-drag cursor-pointer hover:bg-default-blueLight">
-            <span class="no-drag text-xl font-ralewayTiny">x</span>
+          <div id="close" onClick={this.windowClose} class="h-full px-px10 no-drag cursor-pointer hover:bg-default-blueDark dark:hover:bg-default-blueLight">
+            <span class="no-drag text-xl font-ralewayTiny text-default-black dark:text-default-white">x</span>
           </div>
         </nav>
         <router-view />
