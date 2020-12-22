88c2349ac6fe48b01f972e674590ade8039da92f
Novout <novout@hotmail.com>
2020-12-21 22:07:23 -0300

    chore(tailwind): new blue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         dark: '#202e78',
         blueLight: '#b5d6fa',
         blueDark: '#35495e',
+        blueTertiary: '#7dbbf5',
         red: '#FF5252'
       },
       dark: {
