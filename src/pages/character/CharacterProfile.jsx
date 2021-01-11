35792c7fe0c8c04f2840804e5e2f938ee501654e
Novout <novout@hotmail.com>
2021-01-10 21:58:50 -0300

    feat(profile): proficiency value with expertise

--- src/pages/character/CharacterProfile.jsx
+++ src/pages/character/CharacterProfile.jsx
@@ -149,17 +149,22 @@ const ExpertiseItem = defineComponent({
       <>
         {expertises.map((expertise) => {
           return (
-            <article class="flex justify-between items-center mt-:1 bg-dark-one dark:bg-dark-bg hover:bg-dark-oneHover dark:hover:bg-dark-bgHover w-full p-:1">
+            <article class="flex justify-between items-center mt-:1 bg-white-one dark:bg-dark-bg hover:bg-white-oneHover dark:hover:bg-dark-bgHover w-full p-:1">
               <label class="toggle">
                 <input 
                   class="input-custom" 
                   type="checkbox"
-                  vModel={[character.expertises[expertise.code], ['number']]} 
+                  vModel={character.expertises[expertise.code]} 
                 />
                 <span class="label-custom">
                   <span class="input-text-custom">{expertise.name}</span>
                 </span>
               </label>
+              <p 
+                class="pointer-events-none"
+              >
+                {(character.habilityModifier[expertise.type] + (character.expertises[expertise.code] ? character.proficiencyBonus : 0))}
+              </p>
             </article>
           )
         })}
