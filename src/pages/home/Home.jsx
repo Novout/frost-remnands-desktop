b2466bba08cfa4ca20c4bbef0b1cafb497e27ded
Novout <novout@hotmail.com>
2021-01-10 23:47:50 -0300

    feat(link): transition route in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,10 +20,13 @@ export default defineComponent({
           class="bg-center bg-default-white dark:bg-default-black bg-no-repeat bg-cover h-screen w-full overflow-hidden"
           class={background.value}
         >
-          <main class="main flex justify-center items-center flex-col px-:2 bg-none h-medium w-11/12 center">
+          <main class="main flex justify-center items-center flex-col px-:2 bg-none h-medium w-11/12 center transition duration-500 ease-in-out transform hover:-translate-y-1">
             <h1 class="font-poppinsBold text-1wh pb-8 xl:pb-40 mb-:5 pointer-events-none text-default-blueDark dark:text-default-blueTertiary">Restos da Geada</h1>
             {options.map(option => 
-              <router-link to={option.to} class="flex justify-center font-ralewayMedium dark:text-default-white bg-white-one dark:bg-dark-one border-none py-:2 my-:1 w-8/12 text-lg cursor-pointer no-underline shadow-2xl transition-all duration-200 dark:hover:bg-default-blueLight hover:bg-default-blueLight hover:text-dark-one dark:hover:text-default-black ">{option.title}</router-link>
+              <router-link 
+                to={option.to} 
+                class="flex justify-center font-ralewayMedium dark:text-default-white bg-white-one dark:bg-dark-one border-none py-:2 my-:1 w-8/12 text-lg cursor-pointer no-underline shadow-2xl dark:hover:bg-default-blueLight hover:bg-default-blueLight hover:text-dark-one dark:hover:text-default-black transition duration-300 ease-in-out transform hover:-translate-y-2"
+              >{option.title}</router-link>
             )}
           </main>
         </section>
